699e87c9095295f78abdb3fc1134ac691bf87bad
kwanyong_kim <rhksdyddyd@gmail.com>
2025-01-29 00:55:53 +0900

    [feat] SimpleTabs 추가 - checkbox로 간단하게 tab을 구성 [IMS] N/A

---
+++ src/app/read/1/page.tsx
@@ -0,0 +1,21 @@
+import './tabs.css';
+
+export default function Home() {
+  return (
+    <section className="tabs-root-section">
+      <div className="tabs-container">
+        <div className="tabs">
+          <input type="radio" id="tab1" name="tab" defaultChecked />
+          <label htmlFor="tab1">Tab1</label>
+          <input type="radio" id="tab2" name="tab" />
+          <label htmlFor="tab2">Tab2</label>
+          <input type="radio" id="tab3" name="tab" />
+          <label htmlFor="tab3">Tab3</label>
+          <div className="tc c1">Content for Tab1</div>
+          <div className="tc c2">Content for Tab2</div>
+          <div className="tc c3">Content for Tab3</div>
+        </div>
+      </div>
+    </section>
+  );
+}
